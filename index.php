09460e432d181cab6cbeafad55c56bbd973d0462
Matthaus Litteken <mlitteken@host-245-151.pubnet.pdx.edu>
2009-06-29 18:51:20 -0700

    Added header and footer to craft consistent layout and style.

--- index.php
+++ index.php
@@ -1,18 +1,18 @@
-<link rel="stylesheet" type="text/css" href="includes/layout.css" media="all" />
 <?php
-require_once('includes/config.php');
-require_once('includes/mysqli_connect.php');
+require_once('./includes/header.html');
 ?>
-
 <div class="quadrant" id="onepoint1">
-<?php require_once('modules/owner.php'); ?>
+<?php require_once('./modules/owner.php'); ?>
 </div>
 <div class="quadrant" id="onepoint2">
 1.2
 </div>
 <div class="quadrant" id="twopoint1">
-<?php require_once('modules/details.php'); ?>
+<?php require_once('./modules/details.php'); ?>
 </div>
 <div class="quadrant" id="twopoint2">
 2.2
 </div>
+<?php
+require_once('./includes/footer.html');
+?>
